a30acb535030eb77fd3e5f27cb3a6ae7f19d6487
elvie23 <elviegildorecpsu@gmail.com>
2024-12-23 01:35:35 +0800

    Update Contact.jsx

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -5,19 +5,22 @@ function Contact() {
   useEffect(() => {
     document.title = "Daily Draft - Contacts";
   });
+
   return (
-    <div className="p-48">
-      <div className="flex justify-center items-center flex-col space-y-9 mb-24">
-        <h1 className="text-7xl font-[Oswald] font-light tracking-widest text-center">
+    <div className="p-6 sm:p-12 lg:p-24">
+      <div className="flex flex-col items-center space-y-6 sm:space-y-8 lg:space-y-9 mb-24">
+        <h1 className="text-4xl sm:text-5xl lg:text-7xl font-[Oswald] font-light tracking-widest text-center">
           You want to share some interesting story with us? Let’s work together!
         </h1>
-        <p className="text-2xl font-[Poppins] font-light text-justify ">
+        <p className="text-xl sm:text-2xl lg:text-2xl font-[Poppins] font-light text-center sm:text-justify">
           Contact Daily Draft to schedule a discovery call, request a quote, or
           get answers to any questions you have about our services.
         </p>
 
-        <div className="flex-1 flex justify-end">
-          <button className="btn2">Contact</button>
+        <div className="flex justify-center w-full">
+          <button className="btn2 mt-6 sm:mt-8 px-12 py-4">
+            Contact
+          </button>
         </div>
       </div>
     </div>
