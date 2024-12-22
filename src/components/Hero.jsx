c10128d4cb2bc09add52f5fe46c049e67209f8d8
elvie23 <elviegildorecpsu@gmail.com>
2024-12-23 01:30:16 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
-
 function Hero() {
   return (
-    <div className='flex justify-center items-center p-36 flex-col space-y-9'>
-
-        <h1 className="text-7xl font-[Oswald] font-light tracking-widest">The Blogs.</h1>
-        <p className="text-2xl font-[Poppins] font-light text-center">You may come across interesting topics that pique your curiosity and are worth reading. Be sure to stay tuned for more insightful content that we’ll be sharing soon!
-        </p>
-    
+    <div className="flex justify-center items-center p-12 sm:p-16 lg:p-36 flex-col space-y-6 sm:space-y-8 lg:space-y-9">
+      <h1 className="text-4xl sm:text-5xl lg:text-7xl font-[Oswald] font-light tracking-wide sm:tracking-wider">
+        The Blogs.
+      </h1>
+      <p className="text-lg sm:text-xl lg:text-2xl font-[Poppins] font-light text-center">
+        You may come across interesting topics that pique your curiosity and are worth reading. Be sure to stay tuned for more insightful content that we’ll be sharing soon!
+      </p>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
